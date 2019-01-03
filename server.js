e49541937d65c87b504604e3c16e68f25ecde123
Tyler Storm <stormtv@mail.uc.edu>
2019-01-03 12:11:44 -0500

    set mqtt transaction type as send

--- server.js
+++ server.js
@@ -272,6 +272,7 @@ const handleLogosCallback = (block) => {
       for (let transaction of block.blocks) {
         transaction.batchBlockHash = block.hash
         transaction.hash = hash.get(transaction)
+        transaction.type = 'send'
         publishBlock(`transaction/${transaction.hash}`, transaction)
         transaction.type = 'receive'
         blocks.createBlock(transaction).then((dbBlock) => {
