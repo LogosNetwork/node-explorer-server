55af0907f44b3cb9bfab899d899b1628a586a21a
Tyler Storm <stormtv@mail.uc.edu>
2019-02-19 16:30:07 -0500

    update stresser

--- stresser.js
+++ stresser.js
@@ -1,22 +1,22 @@
-const config = require('./config.json')
-const redis = require('redis')
-const client = redis.createClient()
-const Logos = require('@logosnetwork/logos-rpc-client')
-const RPC = new Logos({ url: `http://${config.delegates[0]}:55000`, debug: false })
-const bigInt = require('big-integer')
+const LogosWallet = require('@logosnetwork/logos-webwallet-sdk')
 const mqtt = require('mqtt')
-const mqttRegex = require('mqtt-regex') // Used to parse out parameters from wildcard MQTT topics
+const bigInt = require('big-integer')
+let totalCount = 0
 let transactionCountLastSecond = 0
+let peakTPS = 0
+let transactionCountLastFiveSeconds = 0
+let transactionCountLast30Seconds = 0
+let spammerPrivateKey = '043B086F030037A6066AEE4B33F6DE1B1ED8CDA2C63B82DB1148E050853A02EB'
+let spamAccounts = 1000
 // MQTT Client
-const broadcastMqttRegex = mqttRegex('account/+account').exec
 let mqttClient = null
 const connectMQTT = () => {
   mqttClient = mqtt.connect('wss:pla.bs:8443')
   mqttClient.on('connect', () => {
     console.log('RPC Callback connected to MQTT server')
-    mqttClient.subscribe('#', (err) => {
+    mqttClient.subscribe('transaction/#', (err) => {
       if (!err) {
-        console.log(`subscribed to #`)
+        console.log(`subscribed to transaction/#`)
       } else {
         console.log(err)
       }
@@ -25,177 +25,101 @@ const connectMQTT = () => {
 
   // Listen for confirmation
   mqttClient.on('message', (topic, message) => {
-    let params = broadcastMqttRegex(topic)
     message = JSON.parse(message.toString())
-    if (params && message.type === 'send') {
-      transactionCountLastSecond++
-      if (accountKeys[message.account] && accountKeys[message.account].pending) {
-        accountKeys[message.account].pending = false
-      }
-    }
+    transactionCountLastSecond++
+    transactionCountLastFiveSeconds++
+    totalCount++
+    transactionCountLast30Seconds++
   })
 }
 
-if (config.resetAccountKeys) client.set('accountKeys', '{}')
-let accountKeys = {}
-let keys = []
-client.get('accountKeys', (err, result) => {
-  if (err) {
-    accountKeys[config.accountID] = {
-      privateKey: config.faucetPrivateKey,
-      publicKey: config.accountKey,
-      address: config.accountID,
-      previous: null,
-      balance: null,
-      pending: false
-    }
-    keys = Object.keys(accountKeys)
-  } else {
-    if (result !== null && result !== '{}') {
-      accountKeys = JSON.parse(result)
-      keys = Object.keys(accountKeys)
+const start = async function() {
+  const Wallet = LogosWallet.Wallet
+  let wallet = new Wallet({
+      password: 'password', // Make this strong
+      autoBatchSends: true
+  });
+  let myRPC = wallet.rpc
+  myRPC.proxy = false
+  wallet.rpc = myRPC
+  let loadAccount = await wallet.createAccount({
+      privateKey: spammerPrivateKey
+  });
+  console.log('creating accounts')
+  let totalFees = bigInt(spamAccounts+1).times(bigInt('10000000000000000000000'))
+  let splitBalance = bigInt(loadAccount.balance).minus(totalFees).divide(spamAccounts+1)
+  console.log('start')
+  for (let i = 0; i < spamAccounts; i++) {
+    console.log(`Creating ${i}`)
+    let account = await wallet.createAccount({
+      index: i
+    })
+    loadAccount.createSend([{
+      target: account.address,
+      amount: splitBalance
+    }], true, wallet.rpc)
+    console.log(`Sent to ${i}`)
+  }
+  let accounts = wallet.accounts
+  waitForSpam = () => {
+    if (bigInt(accounts[0].balance).greater(splitBalance.times(bigInt(2)))) {
+      console.log('waiting')
+      setTimeout(waitForSpam, 500)
     } else {
-      accountKeys[config.accountID] = {
-        privateKey: config.faucetPrivateKey,
-        publicKey: config.accountKey,
-        address: config.accountID,
-        previous: null,
-        balance: null,
-        pending: false
+      console.log('SPAMMING')
+      spam()
+    }
+  }
+  spam = () => {
+    for (let i = 0; i < accounts.length; i++) {
+      console.log(i)
+      let sender = accounts[i]
+      let receiver = null
+      if (accounts.length < i+1) {
+        receiver = accounts[i+1]
+      } else {
+        receiver = accounts[0]
       }
-      keys = Object.keys(accountKeys)
+      try {
+        sender.createSend([{
+          target: receiver.address,
+          amount: '1'
+        }], true, wallet.rpc)
+      } catch (error) {
+        console.log(`Send failed from ${sender.address} to ${receiver.address}`)
+        console.log(error)
+      }
     }
+    console.log(`Spam Started ${Date.now()}`)
   }
-  for (let i in keys) {
-    accountKeys[keys[i]].pending = false
-  }
-  console.log(`Starting Test with: ${keys.length} account(s)`)
-})
-const seedMoney = RPC.convert.toReason(1000, 'LOGOS')
-const networkFee = '10000000000000000000000'
-let totalFeesPaid = bigInt(0)
-const sendFakeTransaction = async () => {
-  let senderIndex = 0
-  let receiverIndex = 0
-  let sender = null
-  let receiver = null
-
-  // Calculate who the sender is
-  // Select an existing generated account at random
-  if (keys.length > 1) {
-    senderIndex = Math.floor(Math.random() * Math.floor(keys.length - 1)) + 1
-  }
-  sender = accountKeys[keys[senderIndex]]
-  if (sender.pending) {
-    // console.log(`${sender.address} is still processing a transaction`)
-    return
-  }
-
-  // Calculate who the receiver is
-  // Generate a new account
-  if (keys.length < config.maximumGeneratedAccounts) {
-    // Generate a new account
-    let account = await RPC.key.create()
-    account.balance = '0'
-    account.previous = '0000000000000000000000000000000000000000000000000000000000000000'
-    accountKeys[account.address] = account
-    keys.push(account.address)
-    receiverIndex = keys.length - 1
-  } else {
-    // Select an existing generated account at random
-    if (keys.length > 1) {
-      receiverIndex = Math.floor(Math.random() * Math.floor(keys.length - 1)) + 1
-    }
-  }
-
-  // We don't want to send to ourselves
-  if (senderIndex === receiverIndex) {
-    receiverIndex++
-    if (keys.length - 1 === senderIndex) {
-      let account = await RPC.key.create()
-      account.balance = '0'
-      account.previous = '0000000000000000000000000000000000000000000000000000000000000000'
-      accountKeys[account.address] = account
-      keys.push(account.address)
-    }
-  }
-  receiver = accountKeys[keys[receiverIndex]]
-
-  // Calculate the value to send
-  let frontier = null
-  let balance = null
-  if (sender.balance === null || sender.previous === null) {
-    let val = await RPC.account(sender.privateKey).info()
-    if (val.error) {
-      balance = '0'
-      frontier = '0000000000000000000000000000000000000000000000000000000000000000'
-    } else {
-      balance = val.balance
-      frontier = val.frontier
-    }
-  } else {
-    balance = sender.balance
-    frontier = sender.previous
-  }
-  // Take balance in reason subtract transaction fee and then divide by a random number 1-50
-  let sendAmount = bigInt(balance).minus(bigInt(networkFee)).divide(Math.floor(Math.random() * Math.floor(10)) + 1)
-  if (sendAmount.greater(seedMoney)) {
-    sendAmount = seedMoney
-  } else if (sendAmount.lesserOrEquals(0)) {
-    // console.log('Skipping Empty account')
-    return
-  }
-
-  // Determine the proper delegate to send to
-  let delegateId = null
-  if (frontier !== '0000000000000000000000000000000000000000000000000000000000000000') {
-    delegateId = parseInt(frontier.slice(-2), 16) % 32
-  } else {
-    delegateId = parseInt(sender.publicKey.slice(-2), 16) % 32
-  }
-  RPC.changeServer(`http://${config.delegates[delegateId]}:55000`)
-
-  // Send block to the delegate
-  sender.pending = true
-  let block = await RPC.account(sender.privateKey).send(sendAmount, receiver.address, frontier, 'reason')
-  //   console.log(`Hash: ${block.hash}
-  // Sent ${RPC.convert.fromReason(sendAmount, 'LOGOS')}λ
-  // From ${sender.address}
-  // To ${receiver.address}
-  // ========================`)
-  if (block.hash) {
-    // Successful sent into consensus
-    sender.balance = bigInt(balance).minus(sendAmount).minus(networkFee).toString()
-    totalFeesPaid = totalFeesPaid.plus(networkFee)
-    receiver.balance = bigInt(receiver.balance).plus(sendAmount).toString()
-    sender.previous = block.hash
-    client.set('accountKeys', JSON.stringify(accountKeys))
-  }
-}
-let continueLoop = true
-const loop = () => {
-  let rand = Math.round(Math.random() * config.fakeTransactionsMaximumInterval) + config.fakeTransactionsMinimumInterval
-  setTimeout(() => {
-    sendFakeTransaction()
-    if (continueLoop) loop()
-  }, rand)
+  waitForSpam()
 }
 const watchTPS = () => {
   setTimeout(() => {
-    console.log(`TPS: ${transactionCountLastSecond} | Accounts: ${keys.length}`)
+    console.log(`TPS: ${transactionCountLastSecond}`)
+    if (transactionCountLastSecond > peakTPS) peakTPS = transactionCountLastSecond
     transactionCountLastSecond = 0
-    if (continueLoop) watchTPS()
+    watchTPS()
   }, 1000)
 }
-if (config.fakeTransactions) {
+const watchTPFive = () => {
   setTimeout(() => {
-    loop()
-    watchTPS()
+    console.log(`TP Five: ${Math.floor(transactionCountLastFiveSeconds / 5)}`)
+    transactionCountLastFiveSeconds = 0
+    watchTPFive()
   }, 5000)
 }
+const watchTPThirty = () => {
+  setTimeout(() => {
+    console.log(`TP Thirty: ${Math.floor(transactionCountLast30Seconds / 30)}`)
+    transactionCountLast30Seconds = 0
+    watchTPThirty()
+  }, 30000)
+}
+
 // Want to notify before shutting down
 const handleAppExit = (options, err) => {
-  console.log(`Thanks for using the Logos Stress Tester you burnt ${RPC.convert.fromReason(totalFeesPaid, 'LOGOS')}λ in fees`)
+  console.log(`Thanks for using the Logos Stress Tester you sent ${totalCount} transactions peak TPS was ${peakTPS}`)
   if (err) {
     console.log(err.stack)
   }
@@ -224,3 +148,9 @@ const configureSignals = () => {
 }
 configureSignals()
 connectMQTT()
+setTimeout(() => {
+  watchTPS()
+  watchTPFive()
+  watchTPThirty()
+  start()
+}, 1000)
